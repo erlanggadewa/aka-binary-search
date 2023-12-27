d16bac00526907407db6f8409c190db0447d6ad8
erlanggadewa <erlanggadewa7@gmail.com>
2023-12-27 21:51:24 +0700

    initial commit

---
+++ src/app/api/aka/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  try {
+    let timeIteration = new Array();
+
+    await timeIteration.push(await runningIteration(2, 1));
+    await timeIteration.push(await runningIteration(4, 1));
+    await timeIteration.push(await runningIteration(16, 1));
+    await timeIteration.push(await runningIteration(32, 1));
+    await timeIteration.push(await runningIteration(64, 1));
+    await timeIteration.push(await runningIteration(128, 1));
+    await timeIteration.push(await runningIteration(256, 1));
+    await timeIteration.push(await runningIteration(518, 1));
+    await timeIteration.push(await runningIteration(1024, 1));
+
+    return Response.json({ data: timeIteration });
+  } catch (error) {
+    throw new Error();
+  }
+}
+
+async function runningIteration(total: number, target: any) {
+  const arr = [];
+  for (let index = 1; index <= total; index++) {
+    arr.push(index);
+  }
+
+  const start = performance.now();
+  const result = await binarySearch(arr, target);
+  const end = performance.now();
+
+  return { time: end - start, totalData: total };
+}
+
+async function binarySearch(arr: any[], target: any) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+      return mid; // elemen ditemukan, kembalikan indeksnya
+    } else if (arr[mid] < target) {
+      left = mid + 1; // cari di sebelah kanan
+    } else {
+      right = mid - 1; // cari di sebelah kiri
+    }
+  }
+
+  return -1; // elemen tidak ditemukan
+}
---
+++ src/components/homepage.tsx
@@ -0,0 +1,103 @@
+"use client";
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
+import { useEffect, useState } from "react";
+// ESM
+import { Line } from "react-chartjs-2";
+
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/aka");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+function HomePage() {
+  const [labels, setLabels] = useState<any[]>();
+  const [time, setTime] = useState<any[]>();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/aka")
+      .then((res) => res.json())
+      .then((res) => {
+        const { data } = res;
+        // Extracting 'totalData' from 'data' and creating new arrays for labels and time
+        const updatedLabels = data.map((x: any) => x.totalData);
+        const updatedTime = data.map((x: any) => x.time);
+
+        // Updating the state once after all transformations
+        setLabels(updatedLabels);
+        setTime(updatedTime);
+      });
+  }, []);
+  console.log("🚀 ~ file: homepage.tsx:47 ~ HomePage ~ time:", time);
+  console.log("🚀 ~ file: homepage.tsx:47 ~ HomePage ~ labels:", labels);
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
+  // const labels = dataFetch.map((x: any) => x.totalData);
+  const data = {
+    labels: labels,
+    datasets: [
+      // {
+      //   label: "Dataset 1",
+      //   data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+      //   borderColor: "rgb(255, 99, 132)",
+      //   backgroundColor: "rgba(255, 99, 132, 0.5)",
+      //   fill: false,
+      //   tension: 0.4,
+      // },
+      {
+        label: "Iteration Binary Search",
+        data: time,
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        fill: false,
+        tension: 0.4,
+      },
+    ],
+  };
+  // const posts = await fetch("http://localhost:3000/").then((res) => res.json());
+  // console.log("🚀 ~ file: homepage.tsx:3 ~ HomePage ~ posts:", posts);
+  return (
+    <div className="h-96">
+      <Line options={options} data={data} className="h-20" />
+    </div>
+  );
+}
+
+export default HomePage;
