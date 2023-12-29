390f115640f8c108ac15571d38a291eaaa87b5e7
erlanggadewa <erlanggadewa7@gmail.com>
2023-12-30 01:19:11 +0700

    update

--- src/app/api/aka/route.ts
+++ src/app/api/aka/route.ts
@@ -5,6 +5,8 @@ const arr = Array.from({ length: 262144 }, (_, index) => index);
 export async function GET(request: NextRequest) {
   try {
     const iterationPromises = [
+      runningIteration(256, 0),
+      runningIteration(512, 0),
       runningIteration(1024, 0),
       runningIteration(2048, 0),
       runningIteration(4096, 0),
@@ -16,6 +18,8 @@ export async function GET(request: NextRequest) {
       // runningIteration(262144, 0),
     ];
     const recursivePromises = [
+      runningRecursive(256, 0),
+      runningRecursive(512, 0),
       runningRecursive(1024, 0),
       runningRecursive(2048, 0),
       runningRecursive(4096, 0),
